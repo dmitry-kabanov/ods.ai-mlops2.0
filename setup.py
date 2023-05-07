4c24529c853022a4471de385ec8e7048c668c388
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2023-05-07 16:47:41 +0200

    Reformat files with black

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Birds object detection project for MLOps 2.0 course',
-    author='Dmitry I. Kabanov',
-    license='MIT',
+    version="0.1.0",
+    description="Birds object detection project for MLOps 2.0 course",
+    author="Dmitry I. Kabanov",
+    license="MIT",
 )
