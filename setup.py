31a1ab712d769caf2422cbf69204ec8610455da6
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2023-05-14 20:58:26 +0000

    Update packaging via pip/setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name="src",
-    packages=find_packages(),
-    version="0.1.0",
-    description="Birds object detection project for MLOps 2.0 course",
-    author="Dmitry I. Kabanov",
-    license="MIT",
-)
+setup()
